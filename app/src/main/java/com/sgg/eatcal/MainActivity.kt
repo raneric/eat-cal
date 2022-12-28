b1ac1ea5c0d4c904b943d2270ec81b5a1b15a1df
Eric RANDRIANARISOA <raneric@gmail.com>
2022-12-28 23:06:08 +0300

    base list stared

--- app/src/main/java/com/sgg/eatcal/MainActivity.kt
+++ app/src/main/java/com/sgg/eatcal/MainActivity.kt
@@ -2,10 +2,26 @@ package com.sgg.eatcal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.sgg.eatcal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHost.navController)
+
+        val appBarConfig = AppBarConfiguration(navHost.navController.graph)
+        binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.primaryTextColor))
+        binding.toolbar.setupWithNavController(navHost.navController, appBarConfig)
     }
 }
